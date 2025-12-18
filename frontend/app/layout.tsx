777bec05351855a0f5573f0439adbff0942de75e
yg1110 <younggil9488@gmail.com>
2025-12-18 17:54:15 +0900

    feat: add next + shadcn

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import Providers from "@/config/providers";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
