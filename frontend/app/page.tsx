860a498ce17fe44c79e95a62ec6d5cb42a200ba3
yg1110 <younggil9488@gmail.com>
2025-12-19 12:33:33 +0900

    feat: 로그인, 회원가입 로직 추가

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,26 @@
-export default function Home() {
+import { auth } from "@/auth";
+import { signOut } from "@/auth";
+import Link from "next/link";
+
+export default async function Home() {
+  const session = await auth();
+
   return (
-    <div>main</div>
+    <div>
+      <p>현재 로그인한 유저 보여주기</p>
+      <p>이메일 = {session?.user?.email}</p>
+      {session?.user ? (
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button type="submit">로그아웃</button>
+        </form>
+      ) : (
+        <Link href="/signin">로그인</Link>
+      )}
+    </div>
   );
 }
